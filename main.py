07d302577a21088ca9daa3b37bd967e364ca9fad
Mahdi <mahdi.kazemimajd@gmail.com>
2024-02-06 08:23:55 +0100

    Implement the very first and simple API

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from langchain_helper import agent_executer
+
+app = FastAPI()
+
+@app.get("/clearphone")
+def clear_phone(column_name: str = 'phones'):
+    query = f'I have a data frame with a phone number column with the name of "{column_name}". phone numbers in this column are not in a correct format and I want to have javascript code to discard leading zeros and replace them with plus sign.'
+    result = agent_executor.invoke({'input': query})
+    return result
